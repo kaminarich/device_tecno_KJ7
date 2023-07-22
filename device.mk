a79fd01343cd0f3b8b6e723617b3c1fdd1b53eb1
bengris32 <bengris32@protonmail.ch>
2023-07-22 09:15:05 +0100

    KJ7: Switch to AOSP Boot implementation
    * We can no longer use the MediaTek boot control implementation
      due to legal issues. Switch to the AOSP one.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -100,8 +100,8 @@ PRODUCT_PACKAGES += \
 # Boot
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-impl:64 \
+    android.hardware.boot@1.2-impl.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
