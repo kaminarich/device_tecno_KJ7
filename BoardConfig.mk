0574c849e0d24075abe7b1c625ba213c182259b6
bengris32 <bengris32@protonmail.ch>
2023-10-26 09:16:36 +0530

    KJ7: Enable support for F2FS formatted userdata

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,7 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/etc/fstab.mt6789
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
