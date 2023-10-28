d5cc6f85c67863f80970140e6f5a16ee0f82708c
Mashopy <eliasgheeraert@gmail.com>
2023-10-28 14:15:33 +0530

    KJ7: Setup OSS audio service
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I2b5ea9b12f78db5a179268a9b3b1b440991fd091

--- device.mk
+++ device.mk
@@ -51,7 +51,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@7.0-impl
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl
