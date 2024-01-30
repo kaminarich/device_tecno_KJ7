5a7ad2523fd78088d1877b90aad871df61067e16
Woomymy <woomy@woomy.be>
2024-01-30 04:55:00 +0300

    KJ7: Use 64-bits C2 service
    * Works as fine as 32-bits one
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I7bc9ae678af2acb2c515bbb6b8622af3118302a7

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup {
     case "$1" in
+        vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
+            sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
+            ;;
         vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml)
             sed -i 's/1.1/1.2/' "$2"
             ;;
