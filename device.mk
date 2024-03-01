a9a459d8d5aabbb3c5f408f43bcac9a32ee3395e
bengris32 <bengris32@protonmail.ch>
2024-03-01 20:53:37 +0530

    KJ7: Build `MtkInCallService`

--- device.mk
+++ device.mk
@@ -74,7 +74,8 @@ PRODUCT_PACKAGES += \
     libunwindstack.vendor
 
 PRODUCT_PACKAGES += \
-    BesLoudness
+    BesLoudness \
+    MtkInCallService
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
