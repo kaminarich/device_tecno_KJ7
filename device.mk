c47d7ec32fb4de2e5acbf9122ad7d475f926a602
Sarthak Roy <sarthakroy2002@gmail.com>
2024-03-10 17:59:13 +0530

    KJ7: Add Priv-app permissions for hotword
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I60742841306a42926f7adaa53536ef093d871bbf

--- device.mk
+++ device.mk
@@ -369,6 +369,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.android.hotwordenrollment.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.android.hotwordenrollment.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-V2-ndk.vendor \
