830a80ef4a752550ca460d84eaa9d43848cc673b
LuK1337 <priv.luk@gmail.com>
2024-03-13 21:58:48 +0530

    KJ7: Set a default Settings.Global.DEVICE_NAME
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16

--- device.mk
+++ device.mk
@@ -311,6 +311,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ApertureOverlayKJ7 \
     FrameworksResOverlayKJ7 \
+    SettingsProviderOverlayKJ7 \
     SystemUIOverlayKJ7 \
     TelephonyOverlayKJ7 \
     TetheringConfigResOverlayKJ7 \
