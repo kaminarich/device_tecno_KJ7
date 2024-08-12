ea13bb2386c1218f0e88958abed8e05fab6ab5d7
AndroidHQ254 <d.murimi14@gmail.com>
2024-08-12 20:14:10 +0300

    KJ7: Enable virtual A/B OTA compression
    Update device.mk

--- device.mk
+++ device.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit virtual_ab_ota product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+# Enable virtual A/B OTA compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
