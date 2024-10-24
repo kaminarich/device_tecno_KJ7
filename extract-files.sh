3defb7f1c9203a104442234f183390fd5066555d
bengris32 <bengris32@protonmail.ch>
2024-10-25 01:33:34 +0800

    KJ7: Switch to Pixel Thermal HAL
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Change-Id: I92fcc16ce01aae8b1b9307c38f4236dffb7f78b9
    Co-Authored-By: Woomymy <woomy@woomy.be>
    Co-Authored-By: Mashopy <eliasgheeraert@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -107,6 +107,10 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
             sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
             ;;
+        vendor/etc/init/init.thermal_core.rc)
+            [ "$2" = "" ] && return 0
+            sed -i 's|ro.vendor.mtk_thermal_2_0|vendor.thermal.link_ready|g' "${2}"
+           ;;
         vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml)
             [ "$2" = "" ] && return 0
             sed -i 's/1.1/1.2/' "$2"
