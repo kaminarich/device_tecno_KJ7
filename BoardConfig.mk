73609357f2bfa44b3e8a926f2df38f800780a7e3
Dennis Murimi <d.murimi14@gmail.com>
2025-02-19 03:33:11 +0300

    KJ7: Generate FCM for reported HALs
    * Inherit common Mediatek FCM while here

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,10 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 
 # Vintf
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
