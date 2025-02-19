8273ab8e55ebbfa71f4bfff3926c8089fa2fa588
Dennis Murimi <d.murimi14@gmail.com>
2025-02-19 20:58:07 +0300

    KJ7: Use libhidlbase-v31 for camerahalserver
    Abort message: 'incStrongRequireStrong() called on 0xb400007a0f4625b0 which isn't already owned'

--- device.mk
+++ device.mk
@@ -393,6 +393,9 @@ PRODUCT_PACKAGES += \
     libhidlbase-v32.vendor \
     libutils-v32
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v31/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v31.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,9 @@ function blob_fixup {
             "$PATCHELF" --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "$2"
             "$PATCHELF" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "$2"
             ;;
+        vendor/bin/hw/mt6789/camerahalserver)
+            "$PATCHELF" --replace-needed "libutils.so" "libutils-v31.so" "$2"
+            ;;
         vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service |\
         vendor/lib64/android.hardware.power-service-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "$2"
