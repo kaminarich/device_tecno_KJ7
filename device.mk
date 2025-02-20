5fad252f118cd52350cf353800903bb5e93eb107
Bruno Martins <bgcngm@gmail.com>
2025-02-20 19:38:40 +0300

    KJ7: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -119,7 +119,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor \
     libdrm.vendor \
     libmockdrmcryptoplugin
