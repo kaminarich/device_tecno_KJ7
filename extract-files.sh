c4e0ce2fa251eed9e5175800fa46f2ba4413d6d7
SamarV-121 <samarvispute121@pm.me>
2025-02-22 01:11:11 +0300

    KJ7: Fix properties with invalid vendor namespace
    Change-Id: I772ae06b5aa729744fac0b2bc19f20d0bda5ff38

--- extract-files.sh
+++ extract-files.sh
@@ -106,6 +106,9 @@ function blob_fixup {
             "$PATCHELF" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
             grep -q "libstagefright_foundation-v33.so" "${2}" || "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/lib*/libMtkOmxCore.so)
+            sed -i "s/mtk.vendor.omx.core.log/ro.vendor.mtk.omx.log\x00\x00/" "$2"
+            ;;
         vendor/lib64/libwifi-hal-mtk.so)
             "$PATCHELF" --set-soname "libwifi-hal-mtk.so" "${2}"
             ;;
