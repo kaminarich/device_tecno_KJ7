3218545ee436747b9cd0b334115e15ffc33538c5
fjrXTR <fajarslebew31@gmail.com>
2025-03-09 14:56:06 +0700

    KJ7: Build Mapper & Required vndk blobs
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -135,10 +135,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor \
-    android.hardware.graphics.allocator@2.0.vendor \
-    android.hardware.graphics.allocator@3.0.vendor \
-    android.hardware.graphics.allocator@4.0.vendor
+    android.hardware.graphics.mapper@4.0.vendor \
+    android.hardware.graphics.allocator@4.0.vendor \
+    libion.vendor \
+    libui.vendor
 
 PRODUCT_PACKAGES += \
     libutilscallstack.vendor
